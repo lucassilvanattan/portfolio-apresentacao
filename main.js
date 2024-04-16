1839c12f6501c865faeec33d340460c75ef6126b
lucassilvanattan <lucassilvanattan@gmail.com>
2024-04-16 18:56:14 -0300

    slvando projeto

---
+++ main.js
@@ -0,0 +1,5 @@
+const home = document.querySelector(".homeBT")
+const habilidades = document.querySelector(".habilidadesBT")
+const contato = document.querySelector(".contatoBT")
+
+console.log(habilidades)
