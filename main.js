b2e1a4ab266be3436c703942ac5ca1671c549bdf
lucassilvanattan <lucassilvanattan@gmail.com>
2024-04-27 18:39:58 -0300

    finalizando pagina 3

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-const img = document.getElementById("gengar");
+document.addEventListener("DOMContentLoaded", function() {
+    const img = document.getElementById("gengar");
+    const imagemProjetoPsiquico = document.querySelector(".projeto__psiquico");
+    const nomeDoProjeto = document.querySelector(".titulo__projeto");
 
-img.addEventListener("mouseenter", () => {
-    img.setAttribute("src", "./assets/gengar-move.gif");
-});
+    img.addEventListener("mouseenter", () => {
+        img.setAttribute("src", "./assets/gengar-move.gif");
+    });
 
-img.addEventListener("mouseleave", () => {
-    img.setAttribute("src", "./assets/gengar.png");
+    img.addEventListener("mouseleave", () => {
+        img.setAttribute("src", "./assets/gengar.png");
+    });
+
+    if (imagemProjetoPsiquico) {
+        imagemProjetoPsiquico.addEventListener("mouseenter", () => {
+            nomeDoProjeto.classList.add("titulo__psiquico");
+        });
+    }
 });
